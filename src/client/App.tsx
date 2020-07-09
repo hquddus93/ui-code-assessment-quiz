7d9f19001252ed955506868cf243a4d5e7070eca
hira <hquddus021@gmail.com>
2020-07-09 15:15:12 -0400

    questions rendering

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,14 +1,18 @@
 import * as React from 'react';
 import QuestionsClass from "./questions";
 
+// function startQuiz() {
+//     return <QuestionsClass compiler="TypeScript" framework="React" />
 
+// }
 
 export const App = () => (
     <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
         <h1>Lucid</h1>
         <h2>Welcome to UI Team code assessment!</h2>
         <QuestionsClass compiler="TypeScript" framework="React" />
-  
+        {/* <button onClick={() => startQuiz()}>Start Quiz</button> */}
+
         
     </div>
 );
--- src/client/questions.tsx
+++ src/client/questions.tsx
@@ -1,31 +1,182 @@
-import React from 'react';
+import * as React from 'react';
 import * as Questions from '../server/data.json';
+const Entities = require('html-entities').AllHtmlEntities;
 
-export interface QuestionsClassProps { 
+
+export interface Props { 
     compiler: string; 
     framework: string;
     
 }
 
-// 1, how many questions per quiz
+export interface Quest {
+    category: string,
+    type: string,
+    difficulty: string,
+    question: string,
+    correct_answer: string,
+    incorrect_answers?: string[];
 
-// export interface QuestionsClassState {
-//     quests: [];
+}
 
-// }
+export interface State {
+    quests: Quest[],
+    currentQuestion: number,
+    quizQuestions: Quest[],
+    submittedAnswers: string[]
+
+}
+
+class QuestionsClass extends React.Component<Props, State> {
+    state: State = {
+        quests: Questions.results, //50
+        currentQuestion: 0,
+        quizQuestions: [],
+        submittedAnswers: []
+    }
+
+    componentDidMount = () => {
+        // gives an array of 10 q's
+        while (this.state.quizQuestions.length < 10) {
+            let num = Math.floor(Math.random() * this.state.quests.length)
+            if (!this.state.quizQuestions.includes(this.state.quests[num])) {
+                this.state.quizQuestions.push(this.state.quests[num])
+            }
+            
+        }
+
+    }
+
+    shuffleAnswers = () => {
+  
+        let question = this.state.quizQuestions[this.state.currentQuestion];
+        let answerChoices = question.incorrect_answers?.concat(question.correct_answer);
+        return answerChoices?.sort().map((ans, idx) => {
+            const entities = new Entities();
+            ans = entities.decode(ans)
+            return (
+            <>
+                <span>
+                    <input type='radio' id={`ans-${idx}`} value={ans} onChange={this.handleChange}/> <label>{ans}</label>
+                    <br />
+                </span>
+            </>
+            )
+
+        });
 
 
-class QuestionsClass extends React.Component<QuestionsClassProps, {}> {
+    }
 
-    render () {
-       const quests = Questions.results.map(quest => quest);
+    multipleChoice = () => {
         debugger
-     
+        const entities = new Entities();
+        let question = entities.decode(this.state.quizQuestions[this.state.currentQuestion].question);
+        return (
+        <div>
+            <h3>{question}</h3>
+            {this.shuffleAnswers()}
+            <br />
+            {this.state.currentQuestion !== 9 ? <button onClick={this.handleClick}>NEXT</button> : <button onClick={this.checkAnswers}>Submit</button>} 
 
-       for (let i=0; i < quests.length; i++){
-           debugger
-               return <p>{quests[i].question}</p>
-       }
+            {/* <button onClick={this.handleClick}>NEXT</button> */}
+        </div>
+        )
+    }
+
+    booleanQuestion = () => {
+        debugger
+        const entities = new Entities();
+        let question = entities.decode(this.state.quizQuestions[this.state.currentQuestion].question);
+        return (
+        <>
+        <p>{question}</p>
+        <span>
+            <input type='radio' id='true' value="true" onChange={this.handleChange}/> <label>True</label>
+        </span>
+        <span>
+            <input type='radio' id='false' value="false" onChange={this.handleChange}/><label>False</label>
+        </span>
+        {this.state.currentQuestion !== 9 ? <button onClick={this.handleClick}>NEXT</button> : <button onClick={this.checkAnswers}>Submit</button>} 
+        
+        {/* <button onClick={this.handleClick}>NEXT</button> */}
+
+        </>)
+
+    }
+
+    shortAnswer= () => {
+        debugger
+        const entities = new Entities();
+        let question = entities.decode(this.state.quizQuestions[this.state.currentQuestion].question);
+        return (
+            <>
+            <p>{question}</p>
+            <span>
+                <input onChange={this.update}/>
+                <button onClick={this.handleChange}>Submit</button>
+            </span>
+                {this.state.currentQuestion !== 9 ? <button onClick={this.handleClick}>NEXT</button> : <button onClick={this.handleSubmit}>Submit</button>} 
+            </>
+        )
+    }
+
+    checkAnswers = () => {
+        let count = 0;
+
+        for (let i = 0; i < this.state.quizQuestions.length; i++) {
+            if (this.state.submittedAnswers[i] === this.state.quizQuestions[i].correct_answer) {
+                count ++;
+            }
+        }
+        debugger
+
+    }
+
+    handleSubmit = () => {
+        // checkAnswers()
+    }
+
+    update = (field:any) => {
+        debugger
+        this.setState({
+            submittedAnswers: this.state.submittedAnswers.concat([field.target.value])
+        })
+
+
+    }
+
+    handleClick = () => {
+        //next button 
+        this.setState({
+            currentQuestion: (this.state.currentQuestion + 1)
+        });
+    }
+
+    handleChange = (event:any) => {
+        debugger
+        this.setState({submittedAnswers: this.state.submittedAnswers.concat([event.target.value])
+        })
+        debugger
+
+    }
+
+
+     render () {
+
+        //  if else statements deciding which type of question to render
+
+
+    this.componentDidMount();
+        debugger
+         let question = this.state.quizQuestions[this.state.currentQuestion];
+         if (question.type === 'multiple') {
+            return this.multipleChoice()
+         } else if (question.type === 'text') {
+            return this.shortAnswer();
+         } else if (question.type === 'boolean') {
+            return this.booleanQuestion();
+         }
 
     };
 };
