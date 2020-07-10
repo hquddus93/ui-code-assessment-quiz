44b3c71bba18eee28bde63170b440c5202baa9ea
hira <hquddus021@gmail.com>
2020-07-09 20:17:04 -0400

    complete

--- src/client/questions.tsx
+++ src/client/questions.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import * as Questions from '../server/data.json';
+const styles = require('../stylesheets/styles.css')
 const Entities = require('html-entities').AllHtmlEntities;
 
 
@@ -24,6 +25,8 @@ export interface State {
     currentQuestion: number,
     quizQuestions: Quest[],
     submittedAnswers: string[]
+    field: string,
+    selectedAnswer: string
 
 }
 
@@ -32,17 +35,18 @@ class QuestionsClass extends React.Component<Props, State> {
         quests: Questions.results, //50
         currentQuestion: 0,
         quizQuestions: [],
-        submittedAnswers: []
+        submittedAnswers: [],
+        field: "",
+        selectedAnswer: ""
     }
 
     componentDidMount = () => {
         // gives an array of 10 q's
-        while (this.state.quizQuestions.length < 10) {
+        while (this.state.quizQuestions.length < 5) {
             let num = Math.floor(Math.random() * this.state.quests.length)
             if (!this.state.quizQuestions.includes(this.state.quests[num])) {
                 this.state.quizQuestions.push(this.state.quests[num])
             }
-            
         }
 
     }
@@ -51,17 +55,20 @@ class QuestionsClass extends React.Component<Props, State> {
         // displays answer choices in alphabetical order
         let question = this.state.quizQuestions[this.state.currentQuestion];
         let answerChoices = question.incorrect_answers?.concat(question.correct_answer);
-        debugger
         return answerChoices?.sort().map((ans, idx) => {
             const entities = new Entities();
             ans = entities.decode(ans)
+            let test = document.querySelectorAll('input[type=radio]');
             debugger
             return (
             <>
                 <span>
-                    <input type='radio' id={`ans-${idx}`} value={ans} onChange={this.handleChange}/> <label>{ans}</label>
+                    
+                    <input type='radio' key={idx} id={`ans-${idx}`} checked={this.state.selectedAnswer === ans} value={ans} name='answerChoice' onChange={this.handleChange}/> <label>{ans}</label>
                     <br />
+
                 </span>
+                    
             </>
             )
 
@@ -71,7 +78,7 @@ class QuestionsClass extends React.Component<Props, State> {
     }
 
     multipleChoice = () => {
-        debugger
+ 
         const entities = new Entities();
         let question = entities.decode(this.state.quizQuestions[this.state.currentQuestion].question);
         return (
@@ -79,68 +86,92 @@ class QuestionsClass extends React.Component<Props, State> {
             <h3>{question}</h3>
             {this.shuffleAnswers()}
             <br />
-            {this.state.currentQuestion !== 9 ? <button onClick={this.handleClick}>NEXT</button> : <button onClick={this.checkAnswers}>Submit</button>} 
+
+            <p id='errors' className='hidden'>Answer is required</p>
+            <br />
+            {this.state.currentQuestion !== this.state.quizQuestions.length - 1 ? <button onClick={this.handleClick}>NEXT</button> : <button onClick={this.checkAnswers}>Submit</button>} 
 
         </div>
         )
     }
 
     booleanQuestion = () => {
-        debugger
+  
         const entities = new Entities();
         let question = entities.decode(this.state.quizQuestions[this.state.currentQuestion].question);
         return (
-        <>
-        <p>{question}</p>
+        <div>
+        <h3>{question}</h3>
         <span>
-            <input type='radio' id='true' value="true" onChange={this.handleChange}/> <label>True</label>
+            <input type='radio' id='true' value="true" name='boolean' checked={this.state.selectedAnswer === 'true'} onChange={this.handleChange}/> <label>True</label>
         </span>
+        <br />
         <span>
-            <input type='radio' id='false' value="false" onChange={this.handleChange}/><label>False</label>
+            <input type='radio' id='false' value="false" name='boolean' checked={this.state.selectedAnswer === 'false'} onChange={this.handleChange}/><label>False</label>
         </span>
-        {this.state.currentQuestion !== 9 ? <button onClick={this.handleClick}>NEXT</button> : <button onClick={this.checkAnswers}>Submit</button>} 
+        <br />
+        <p id='errors' className='hidden'>Answer is required</p>
+        <br />
+
+        {this.state.currentQuestion !== this.state.quizQuestions.length - 1 ? <button onClick={this.handleClick}>NEXT</button> : <button onClick={this.checkAnswers}>Submit</button>} 
         
 
 
-        </>)
+        </div>)
 
     }
 
     shortAnswer= () => {
-        debugger
+
         const entities = new Entities();
         let question = entities.decode(this.state.quizQuestions[this.state.currentQuestion].question);
         return (
-            <>
-            <p>{question}</p>
+            <div>
+            <h3>{question}</h3>
             <span>
-                <input onChange={this.update}/>
+                <input onChange={this.update} placeholder=""/>
             </span>
-                {this.state.currentQuestion !== 9 ? <button onClick={this.handleClick}>NEXT</button> : <button onClick={this.handleSubmit}>Submit</button>} 
-            </>
+            <br />
+            <p id='errors' className='hidden'>Answer is required</p>
+            <br />
+                {this.state.currentQuestion !== this.state.quizQuestions.length - 1 ? <button onClick={this.handleClick}>NEXT</button> : <button onClick={this.checkAnswers}>Submit</button>} 
+            </div>
         )
     }
 
     checkAnswers = () => {
-        let count = 0;
+        this.handleClick();
 
+        let count = 0;
         for (let i = 0; i < this.state.quizQuestions.length; i++) {
             if (this.state.submittedAnswers[i] === this.state.quizQuestions[i].correct_answer) {
-                count ++;
+                count += 1;
             }
         }
-        debugger
+        return (
+            <div>
+                <h3>Summary</h3>
+                 <p>Correct: {count}</p>
+                <p>Wrong: {this.state.quizQuestions.length - count}</p>
+                <p>Final Score: {count / this.state.quizQuestions.length * 100 }%</p>
+                <button onClick={() => window.location.reload()}>Retake Quiz</button>
+            </div>
+        )
+    
+    
+
 
     }
 
-    handleSubmit = () => {
-        // checkAnswers()
+    renderErrors = () => {
+        let error = document.getElementById('errors');
+        error?.classList.remove('hidden');
+        error?.classList.add('errors');
     }
 
     update = (field:any) => {
-        debugger
         this.setState({
-            submittedAnswers: this.state.submittedAnswers.concat([field.target.value])
+            field: field.target.value
         })
 
 
@@ -148,23 +179,40 @@ class QuestionsClass extends React.Component<Props, State> {
 
     handleClick = () => {
     
+        if (this.state.field.length > 0) {
+            this.setState({submittedAnswers: this.state.submittedAnswers.concat(this.state.field)})
+            this.setState({
+                currentQuestion: (this.state.currentQuestion + 1),
+                field: "",
+                selectedAnswer: ""
+            });
+        }
 
-        debugger
+        if (this.state.selectedAnswer.length > 0) {
+            this.setState({submittedAnswers: this.state.submittedAnswers.concat(this.state.selectedAnswer)})
+            this.setState({
+                currentQuestion: (this.state.currentQuestion + 1),
+                field: "",
+                selectedAnswer: ""
+            });
+        }
+
+        if (this.state.selectedAnswer.length === 0 && this.state.field.length === 0 && this.state.currentQuestion < this.state.quizQuestions.length) {
+
+            this.renderErrors();
+        }
+
+
         //next button 
-        this.setState({
-            currentQuestion: (this.state.currentQuestion + 1)
-        });
+   
     }
 
     handleChange = (event:any) => {
         // clicking an answer choice
-        let array = Array.from(event.target.parentElement.childNodes)
 
-
-        debugger
-        this.setState({submittedAnswers: this.state.submittedAnswers.concat([event.target.value])
+        this.setState({selectedAnswer: event.target.value
         })
-        debugger
+
 
     }
 
@@ -175,14 +223,19 @@ class QuestionsClass extends React.Component<Props, State> {
 
 
     this.componentDidMount();
-        debugger
+         let error = document.getElementById('errors');
+         error?.classList.remove('errors');
+         error?.classList.add('hidden');
          let question = this.state.quizQuestions[this.state.currentQuestion];
-         if (question.type === 'multiple') {
+
+         if (question && question.type === 'multiple') {
             return this.multipleChoice()
-         } else if (question.type === 'text') {
+         } else if (question && question.type === 'text') {
             return this.shortAnswer();
-         } else if (question.type === 'boolean') {
+         } else if (question && question.type === 'boolean') {
             return this.booleanQuestion();
+         } else {
+            return this.checkAnswers();
          }
 
     };
